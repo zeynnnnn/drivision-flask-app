2988802531167e2990dc592e3e77bcd6d95e1318
Yusuf Dalva <yusuf.dalva.1@gmail.com>
2020-05-13 20:27:07 +0300

    LaneNet added

--- predict.py
+++ predict.py
@@ -1,5 +1,15 @@
 from flask import Flask, request, jsonify, json
 
+#LaneNet update
+from tensorflow.keras.models import load_model
+from skimage import transform
+from skimage import exposure
+import numpy as np
+import cv2
+import tensorflow as tf
+import glog as log
+from models.lane_detection_lanenet.lanenet_model import lanenet_postprocess, lanenet, global_config
+
 from models.lane_detection.binarization_utils import binarize
 from models.lane_detection.calibration_utils import calibrate_camera, undistort
 from models.lane_detection.globals import xm_per_pix, time_window
@@ -7,6 +17,72 @@ from models.lane_detection.line_utils import get_fits_by_sliding_windows, Line, 
 from models.lane_detection.perspective_utils import birdeye
 from models.vehicle_detectionn.lane import *
 from models.vehicle_detectionn.yolo_pipeline import *
+
+CFG = global_config.cfg
+
+
+def process_lane_recognition_model(image):
+
+    weights_path="models/lane_detection_lanenet/downloaded_model/tusimple_lanenet_vgg.ckpt"
+
+    #preprocess
+    image_vis = image
+    image = cv2.resize(image, (512, 256), interpolation=cv2.INTER_LINEAR)
+    image = image / 127.5 - 1.0
+    log.info('Image load complete')
+
+    #model run
+    tf.compat.v1.disable_eager_execution()
+    input_tensor = tf.compat.v1.placeholder(dtype=tf.float32, shape=[1, 256, 512, 3], name='input_tensor')
+
+    net = lanenet.LaneNet(phase='test', net_flag='vgg')
+    binary_seg_ret, instance_seg_ret = net.inference(input_tensor=input_tensor, name='lanenet_model')
+
+    postprocessor = lanenet_postprocess.LaneNetPostProcessor()
+
+    saver = tf.compat.v1.train.Saver()
+
+    # Set sess configuration
+    sess_config = tf.compat.v1.ConfigProto()
+    sess_config.gpu_options.per_process_gpu_memory_fraction = CFG.TEST.GPU_MEMORY_FRACTION
+    sess_config.gpu_options.allow_growth = CFG.TRAIN.TF_ALLOW_GROWTH
+    sess_config.gpu_options.allocator_type = 'BFC'
+
+    sess = tf.compat.v1.Session(config=sess_config)
+
+    with sess.as_default():
+        saver.restore(sess=sess, save_path=weights_path)
+        binary_seg_image, instance_seg_image = sess.run(
+            [binary_seg_ret, instance_seg_ret],
+            feed_dict={input_tensor: [image]}
+        )
+        postprocess_result = postprocessor.postprocess(
+            binary_seg_result=binary_seg_image[0],
+            instance_seg_result=instance_seg_image[0],
+            source_image=image_vis
+        )
+        # img = np.reshape(binary_seg_image[0] * 255, (binary_seg_image[0].shape[0], binary_seg_image[0].shape[1], 1))
+        # lines = cv2.HoughLines(img, 1, np.pi/180, 200)
+        # print(type(lines))
+        mask_image = postprocess_result['mask_image']
+
+        for i in range(CFG.TRAIN.EMBEDDING_FEATS_DIMS):
+            instance_seg_image[0][:, :, i] = minmax_scale(instance_seg_image[0][:, :, i])
+        embedding_image = np.array(instance_seg_image[0], np.uint8)
+
+    lists = mask_image[:, :, (2, 1, 0)].tolist()
+    json_str = json.dumps(lists)
+    # encoded_img = base64.encodebytes(img_byte_arr.getvalue()).decode('ascii')
+    sess.close()
+    return json_str
+
+
+def minmax_scale(input_arr):
+    min_val = np.min(input_arr)
+    max_val = np.max(input_arr)
+    output_arr = (input_arr - min_val) * 255.0 / (max_val - min_val)
+
+    return output_arr
 
 
 def pipeline_yolo(img):
@@ -103,6 +179,7 @@ def process_pipeline(frame, keep_state=True):
 
     return offset_meter
 
+
 def vehicle_method(img):
     #print(img)
     #newsize = (1280, 720)
@@ -113,22 +190,20 @@ def vehicle_method(img):
     return yolo_result
 
 
-
-
 @app.route('/predict', methods=['POST'])
 def get_prediction():
-    distance_from_center_arr = []
     vehicles =[]
     for key in request.files:
         filestr = request.files[key].read()  # "file" key'i ile gonderilen resmi al
         npimg = np.frombuffer(filestr, np.uint8)
         image = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
-        distance_from_center = process_pipeline(image, keep_state=False)
-        distance_from_center_arr.append(distance_from_center)
+        lane_image = process_lane_recognition_model(image)
+        # distance_from_center = process_pipeline(image, keep_state=False)
+        # distance_from_center_arr.append(distance_from_center)
         vehicles.append(vehicle_method(image))
 
     print(vehicles)
-    return jsonify(distance_from_center_arr=distance_from_center_arr,vehicles=json.dumps(str(vehicles)))
+    return jsonify(vehicles=json.dumps(str(vehicles)))
 
 
 if __name__ == '__main__':
